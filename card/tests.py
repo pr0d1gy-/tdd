3aa194963068f23cabb62a873ef88c99d5d77919
pr0d1gy <kirill.etc@gmail.com>
2017-10-10 14:45:47 +0300

    add chicking card fields

--- card/tests.py
+++ card/tests.py
@@ -6,3 +6,18 @@ class BaseCardTestCase(TestCase):
     def setUp(self):
         from card.models import Card
         self.model = Card
+
+
+class CheckCardFields(BaseCardTestCase):
+
+    def test_card_number_field(self):
+        self.assertTrue(hasattr(self.model, 'number'))
+
+    def test_card_month_field(self):
+        self.assertTrue(hasattr(self.model, 'month'))
+
+    def test_card_year_field(self):
+        self.assertTrue(hasattr(self.model, 'year'))
+
+    def test_card_name_field(self):
+        self.assertTrue(hasattr(self.model, 'name'))
