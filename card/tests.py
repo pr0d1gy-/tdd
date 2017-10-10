0681796831be80c0fa180f5bc36e790a0bf740d5
pr0d1gy <kirill.etc@gmail.com>
2017-10-10 14:44:32 +0300

    add base test

--- card/tests.py
+++ card/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class BaseCardTestCase(TestCase):
+
+    def setUp(self):
+        from card.models import Card
+        self.model = Card
