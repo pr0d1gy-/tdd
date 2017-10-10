4ec740764537d9b1b740de6225728bd6f7be587e
pr0d1gy <kirill.etc@gmail.com>
2017-10-10 16:01:54 +0300

    add test service create

--- card/tests.py
+++ card/tests.py
@@ -1,10 +1,23 @@
+from datetime import datetime, timedelta
+
 from django.test import TestCase
 from django.db import IntegrityError
 
 
-class BaseCardTestCase(TestCase):
+class BaseTestCase(TestCase):
 
     def setUp(self):
+        self.now = datetime.now()
+        self.expire_date = self.now + timedelta(days=50)
+
+    def _get_expire_date(self):
+        return self.expire_date.month, self.expire_date.year
+
+
+class BaseCardTestCase(BaseTestCase):
+
+    def setUp(self):
+        super(BaseCardTestCase, self).setUp()
         from card.models import Card
         self.model = Card
 
@@ -75,5 +88,112 @@ class BaseCardServiceTestCase(BaseCardTestCase):
 
     def setUp(self):
         super(BaseCardServiceTestCase, self).setUp()
-        from card.services import CardService
+        from card.services import CardService, CardServiceException
         self.service = CardService
+        self.service_exception = CardServiceException
+
+
+class ServiceCreateTestCase(BaseCardServiceTestCase):
+
+    def test_wrong_number(self):
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create(None, None, None, None)
+        e = cm.exception
+        self.assertEqual(str(e), '`Number` is required.')
+
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('test', *self._get_expire_date(), 'Test Name')
+        e = cm.exception
+        self.assertEqual(str(e), '`Number` must have only numbers.')
+
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('1111', *self._get_expire_date(), 'Test Name')
+        e = cm.exception
+        self.assertEqual(str(e), '`Number` must have 16 digits.')
+
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('11112222333344445', *self._get_expire_date(), 'Test Name')
+        e = cm.exception
+        self.assertEqual(str(e), '`Number` must have 16 digits.')
+
+        self.service.create('1111222233334444', *self._get_expire_date(),
+                            'Test Name')
+
+    def test_wrong_month(self):
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('1111222233334444', None,
+                                self.expire_date.year, 'Test')
+        e = cm.exception
+        self.assertEqual(str(e), '`Month` is required.')
+
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('1111222233334444', 'test',
+                                self.expire_date.year, 'Test')
+        e = cm.exception
+        self.assertEqual(str(e), '`Month` must have only numbers.')
+
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('1111222233334444', '123',
+                                self.expire_date.year, 'Test')
+        e = cm.exception
+        self.assertEqual(str(e), '`Month` should be in the range 1-12.')
+
+        self.service.create('1111222233334444', *self._get_expire_date(),
+                            'Test Name')
+
+    def test_wrong_year(self):
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('1111222233334444', self.expire_date.month,
+                                None, 'Test')
+        e = cm.exception
+        self.assertEqual(str(e), '`Year` is required.')
+
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('1111222233334444', self.expire_date.month,
+                                'test', 'Test')
+        e = cm.exception
+        self.assertEqual(str(e), '`Year` must have only numbers.')
+
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('1111222233334444', self.expire_date.month,
+                                (self.now.year - 2000) - 5, None)
+        e = cm.exception
+        self.assertEqual(str(e), '`Month` should be in the range {}-99.'.format(
+            (self.now.year - 2000)
+        ))
+
+        self.service.create('1111222233334444', *self._get_expire_date(),
+                            'Test Name')
+
+    def test_wrong_name(self):
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('1111222233334444', *self._get_expire_date(),
+                                None)
+        e = cm.exception
+        self.assertEqual(str(e), '`Name` is required.')
+
+        self.service.create('1111222233334444', *self._get_expire_date(),
+                            'Test Name')
+
+    def test_unique(self):
+        self.service.create('1111222233334444', self.expire_date.month,
+                            self.expire_date.year, 'Test Name')
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('1111222233334444', self.expire_date.month,
+                                self.expire_date.year, 'Test Name')
+        e = cm.exception
+        self.assertEqual(str(e), 'Card with such number already exists.')
+
+        self.service.create('1111222233335555', self.expire_date.month,
+                            self.expire_date.year, 'Test Name')
+
+    def test_validation_date(self):
+        with self.assertRaises(self.service_exception) as cm:
+            self.service.create('1111222233334444', self.now.month-1,
+                                self.now.year, None)
+
+        e = cm.exception
+        self.assertEqual(str(e), 'Expire date must be greater than current.')
+
+        self.service.create('1111222233334444', *self._get_expire_date(),
+                            'Test Name')
