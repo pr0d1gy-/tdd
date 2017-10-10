ce9d25f4714ccfa574182c8e98a5f9ba1edc8e89
pr0d1gy <kirill.etc@gmail.com>
2017-10-10 15:09:02 +0300

    add test model functionality

--- card/tests.py
+++ card/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.db import IntegrityError
 
 
 class BaseCardTestCase(TestCase):
@@ -21,6 +22,53 @@ class CheckCardFieldsTestCase(BaseCardTestCase):
 
     def test_card_name_field(self):
         self.assertTrue(hasattr(self.model, 'name'))
+
+
+class CheckBaseModelFunctions(BaseCardTestCase):
+
+    def test_saving_card(self):
+        card = self.model(
+            number='1111222233334444',
+            month='01',
+            year='19',
+            name='Test Name'
+        )
+        card.save()
+
+        loaded_card = self.model.objects.get(number=card.number)
+        self.assertEqual(card, loaded_card)
+        self.assertEqual(card.number, loaded_card.number)
+        self.assertEqual(int(card.month), loaded_card.month)
+        self.assertEqual(int(card.year), loaded_card.year)
+        self.assertEqual(card.name, loaded_card.name)
+
+    def test_saving_only_unique_card(self):
+        card = self.model(
+            number='1111222233334444',
+            month='01',
+            year='19',
+            name='Test Name'
+        )
+        card.save()
+
+        with self.assertRaises(IntegrityError):
+            card2 = self.model(
+                number='1111222233334444',
+                month='02',
+                year='20',
+                name='Test Name2'
+            )
+            card2.save()
+
+        card3 = self.model(
+            number='1111222233335555',
+            month='01',
+            year='19',
+            name='Test Name'
+        )
+        card3.save()
+
+        self.assertEqual(2, self.model.objects.all().count())
 
 
 class BaseCardServiceTestCase(BaseCardTestCase):
